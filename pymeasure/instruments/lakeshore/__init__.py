2c404c7b385dfcf2b4e546403cce1e479dc4af2d
PhysFNA2 MeasurementPC <c.f.schippers@tue.nl>
2020-01-16 11:01:21 +0100

    Add lakeshore 421 code

--- pymeasure/instruments/lakeshore/__init__.py
+++ pymeasure/instruments/lakeshore/__init__.py
@@ -25,3 +25,4 @@
 from .adapters import LakeShoreUSBAdapter
 from .lakeshore425 import LakeShore425
 from .lakeshore331 import LakeShore331
+from .lakeshore421 import LakeShore421
---
+++ pymeasure/instruments/lakeshore/lakeshore421.py
@@ -0,0 +1,136 @@
+#
+# This file is part of the PyMeasure package.
+#
+# Copyright (c) 2013-2019 PyMeasure Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+from pymeasure.instruments import Instrument
+from pymeasure.instruments.validators import strict_discrete_set, \
+    truncated_discrete_set
+
+from visa import constants as visa_const
+from time import sleep
+import numpy as np
+
+
+class LakeShore421(Instrument):
+    """ 
+    Represents the Lake Shore 421 Gaussmeter and provides
+    a high-level interface for interacting with the instrument.
+
+    .. code-block:: python
+
+        gaussmeter = LakeShore421("GPIB::1")
+
+        gaussmeter.unit = "T"               # Set units to Tesla
+        gaussmeter.auto_range = True        # Turn on auto-range
+        gaussmeter.fast_mode = True         # Turn on fast-mode
+
+    """
+
+    _MULTIPLIERS = {1e3: 'k', 1: '', 1e-3: 'm', 1e-6: 'n'}
+    _RANGES = {35: 1, 350: 2, 3500: 3, 35000: 4}
+
+    field_raw = Instrument.measurement(
+        "FIELD?",
+        """ Return the field in the current units and the current multiplier.
+        """
+    )
+
+    field_multiplier = Instrument.measurement(
+        "FIELDM?",
+        """ Return the field multiplier for the returned magnetic field.
+        """,
+        values=_MULTIPLIERS,
+        map_values=True
+    )
+
+    @property
+    def field(self):
+        """ Return the field in the current units. This function takes into
+        account the field multiplier. Returns np.nan if field is out of range.
+        Might want to implement software auto-range.
+        """
+
+        field_raw = self.field_raw
+
+        if not field_raw == "OL":
+            multiplier = self.field_multiplier
+            field = multiplier * field_raw
+        else:
+            field = np.nan
+
+        return field
+
+    unit = Instrument.control(
+        "UNIT?", "UNIT %s",
+        """ A string property that controls the units used by the gaussmeter.
+        Valid values are G, T, Oe, A/m. """,
+        validator=strict_discrete_set,
+        values=['G', 'T', 'Oe', 'A/m'],
+    )
+
+    range = Instrument.control(
+        "RANGE?", "RANGE %d",
+        """ A floating point property that controls the field range of the
+        meter. Valid values are 35, 350, 3500, and 35000 (in Gauss). """,
+        validator=truncated_discrete_set,
+        values=_RANGES,
+        map_values=True
+    )
+
+    auto_range = Instrument.control(
+        "AUTO?", "AUTO %d",
+        """ A boolean property that controls the auto-range option of the
+        meter. Valid values are True and False. Note that the auto-range is
+        relatively slow and might often not suffice for rapid measurements.
+        """,
+        validator=strict_discrete_set,
+        values={True: 1, False: 0},
+        map_values=True
+    )
+
+    fast_mode = Instrument.control(
+        "FAST?", "FAST %d",
+        """ A boolean property that controls the fast-mode option of the
+        meter. Valid values are True and False. """,
+        validator=strict_discrete_set,
+        values={True: 1, False: 0},
+        map_values=True
+    )
+
+    def __init__(self, adapter, **kwargs):
+        super(LakeShore421, self).__init__(
+            adapter,
+            "Lake Shore 421 Gaussmeter",
+            read_termination='\r',
+            write_termination='\n',
+            **kwargs
+        )
+
+        self.adapter.connection.set_visa_attribute(
+            visa_const.VI_ATTR_ASRL_BAUD, 9600)
+        self.adapter.connection.set_visa_attribute(
+            visa_const.VI_ATTR_ASRL_DATA_BITS, 7)
+        self.adapter.connection.set_visa_attribute(
+            visa_const.VI_ATTR_ASRL_STOP_BITS, visa_const.VI_ASRL_STOP_ONE)
+        self.adapter.connection.set_visa_attribute(
+            visa_const.VI_ATTR_ASRL_PARITY, visa_const.VI_ASRL_PAR_ODD)
